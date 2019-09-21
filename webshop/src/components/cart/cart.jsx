2b02a1655b46352379f69649a8424be67da427e7
Razvan Turturica <razvan.turtu@yahoo.com>
2019-09-21 09:50:53 +0300

    Add react-router location changing

--- webshop/src/components/cart/cart.jsx
+++ webshop/src/components/cart/cart.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Reflux from 'reflux';
 import CartStore from '../../stores/cart.js';
 import {CartActions} from '../../actions/cart.js';
+import {withRouter} from 'react-router-dom';
 
 // Reflux.Component is just a wrapper for React.Component to add stores
 // more easily
@@ -25,9 +26,25 @@ class Cart extends Reflux.Component{
         });
     }
 
+    goto(newLocation){
+        // This method will redirect you to the newLocation path
+        // This requires the withRouter import at the top of the file otherwise
+        // the history prop need to be passed from the BrowserRouter component
+        // to the bottom in every component, which is a pain in the code
+        const history = this.props.history;
+        // The hisory is a list of paths which you visited, pushing something
+        // into it will change the location in the url bar and rerender the
+        // BrowserRouter top-level component (meaning that all the subcomponents
+        // will rerender, but the page will not be browser refreshed)
+        // https://reacttraining.com/react-router/web/api/history
+        history.push(newLocation);
+    }
+
     render(){
         return(
             <div>
+                <button onClick={()=>{this.goto('/')}}>Home</button>
+                <button onClick={()=>{this.goto('/shop')}}>Shop</button>
                 <button onClick={()=>{this.addItem()}}>Add Item</button>
                 {this.state.items.map( item => <p>{item}</p> )}
             </div>
--- webshop/src/components/shop/shop.jsx
+++ webshop/src/components/shop/shop.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Shop extends React.Component{
     constructor(props){
@@ -10,6 +11,12 @@ class Shop extends React.Component{
     render(){
         return(
             <div>
+                {/* This is a jsx html comment */}
+                {/* You can also use Link from react-router-dom (imported above) */}
+                {/* to redirect you to a specific path without browser refresh, */}
+                {/* but doing it programatically I believe it has more uses */}
+                <Link to='/'>Home</Link> <br /> 
+                <Link to='/cart'>Cart</Link>  <br />
                 Shop
             </div>
         )
